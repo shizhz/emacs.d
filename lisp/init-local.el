0d422c9614446561ae08d8d7f6f1dc11e2afb02a
Shizhz <messi.shizz@gmail.com>
2022-06-28 22:34:31 +0800

    evil mode - keybindings

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,17 +2,18 @@
 
 ; protobuf-mode
 (require-package 'protobuf-mode)
-
                                         ; evil-mode
 (unless (package-installed-p 'evil)
   (package-install 'evil))
 
-(require 'evil)
 (setq evil-want-C-u-scroll t)
+(setq evil-want-Y-yank-to-eol t)
+(require 'evil)
 (evil-mode 1)
 
-
-// My key-bindings
-
+;;; Evil  keybindings
+(define-key evil-normal-state-map (kbd "SPC SPC") 'execute-extended-command)
+(define-key evil-insert-state-map (kbd "C-e") 'evil-end-of-line-or-visual-line)
+(define-key evil-insert-state-map (kbd "C-a") 'evil-beginning-of-visual-line)
 
 (provide 'init-local)
