3fa2cd06c6bf586230ae93dcbbb597a8785d406d
Shizhz <messi.shizz@gmail.com>
2022-06-28 21:07:06 +0800

    Add evil mode

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -3,8 +3,16 @@
 ; protobuf-mode
 (require-package 'protobuf-mode)
 
-; evil-mode
-;(require-package 'evil)
-;(evil-mode 1)
+                                        ; evil-mode
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+(require 'evil)
+(setq evil-want-C-u-scroll t)
+(evil-mode 1)
+
+
+// My key-bindings
+
 
 (provide 'init-local)
